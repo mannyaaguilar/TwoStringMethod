98a7dd7660a092806ec8e4d7f638fce5e86aaa9b
Manny Aguilar <mannyaaguilar@yahoo.com>
2019-02-13 04:53:44 -0700

    first commit

---
+++ src/com/manny/Main.java
@@ -0,0 +1,11 @@
+package com.manny;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 Student s1 = new Student("Manny", 38);
+	 Student s2 = new Student("Stella", 32);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
